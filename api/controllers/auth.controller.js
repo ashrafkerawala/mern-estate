a5aa14541c5d24ad2aa5842050f48edead5e07f5
ashrafkerawala <ashrafkerawala17@gmail.com>
2023-10-21 15:56:46 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -14,4 +15,23 @@ const signup = async (req, res, next) => {
     }
 }
 
-export { signup }
+const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email })
+        if(!validUser) return next(404, 'Incorrect email or password')
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return next(401, 'Incorrect email or password')
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const { password: pass, ...otherInfo } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(otherInfo)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export { signup, signin }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,5 +4,6 @@ import * as controller from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post('/signup', controller.signup)
+router.post('/signin', controller.signin)
 
 export default router
