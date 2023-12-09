624521be4fe2a21eebc88896619f32431f21fa2a
ashrafkerawala <ashrafkerawala17@gmail.com>
2023-12-09 13:23:41 +0530

    Added get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,5 +8,6 @@ router.get('/', controller.base)
 router.get('/test', controller.test)
 router.post('/update/:id', verifyToken, controller.updateUser)
 router.delete('/delete/:id', verifyToken, controller.deleteUser)
+router.get('/listings/:id', verifyToken, controller.getUserListings)
 
 export default router
