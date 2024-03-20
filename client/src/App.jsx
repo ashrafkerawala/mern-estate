e221620104f26a49899883e892f3bb43cd5a0ca4
ashrafkerawala <ashrafkerawala17@gmail.com>
2024-03-21 00:31:48 +0530

    Completed about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-import About from './pages/about'
 import Profile from './pages/Profile'
 import Header from './components/Header'
 import CreateListing from './pages/CreateListing'
@@ -10,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
 import { Search } from './pages/Search'
+import About from './pages/About'
 
 function App() {
   return (
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,14 @@
-function About() {
+const About = () => {
   return (
-    <h1>About</h1>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl text-slate-700 font-bold mb-4'>About Real Estate</h1>
+      <p className='text-slate-600 mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus enim suscipit doloribus praesentium? Dignissimos quos esse vitae, eum fugiat similique suscipit, tempore laborum explicabo necessitatibus eos hic repudiandae quis qui?
+      Magnam reprehenderit expedita vero sit aut. Dicta, hic, voluptas dolorem perferendis eos reiciendis quasi aspernatur commodi laudantium laboriosam error vitae, nesciunt quaerat quos explicabo alias! Nesciunt odit vero nemo accusantium.</p>
+      <p className='text-slate-600 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestias sequi tempora sunt pariatur laborum in consequuntur. Tenetur deserunt at illum fugit vel, a obcaecati nulla, nihil voluptate accusamus repudiandae.
+      Enim eius molestias velit quos quasi? Rerum modi dolor error labore exercitationem. Distinctio adipisci perferendis quibusdam aliquid dignissimos deleniti quia reiciendis, iusto amet incidunt vero veniam ut magnam aliquam quidem?
+      Sit nobis obcaecati voluptatem provident? Porro fuga mollitia asperiores, architecto in aut sint quod, dicta saepe sed ab praesentium deleniti dignissimos modi a nihil. Quaerat illo asperiores quod sunt aliquam.</p>
+      <p className='text-slate-600 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatibus quis, voluptas cupiditate dicta inventore, doloremque iste natus necessitatibus provident eligendi pariatur tempora consequatur perspiciatis cumque blanditiis. Hic, voluptatum exercitationem?</p>
+    </div>
   )
 }
 
